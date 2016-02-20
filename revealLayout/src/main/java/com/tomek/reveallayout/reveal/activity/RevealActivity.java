3a1785075aba4e0e0598e7fc88d83f293cc97e38
Tomek <tomek.stasiak@gmail.com>
2016-02-20 13:09:08 +0100

    initial commit

---
+++ revealLayout/src/main/java/com/tomek/reveallayout/reveal/activity/RevealActivity.java
@@ -0,0 +1,29 @@
+package com.tomek.reveallayout.reveal.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.tomek.reveallayout.R;
+import com.tomek.reveallayout.reveal.utils.activity.BaseActivity;
+
+public class RevealActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+}
